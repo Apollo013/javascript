0bcee185f24c8e3725d6a06cb7f7b651dd47ae54
@Apollo013 <paul.j.millar@gmail.com>
2024-02-01 07:25:32 +0000

    Add generator function and iterator example

---
+++ concepts/generatorFunction.js
@@ -0,0 +1,25 @@
+const getFunction = function* () {
+    yield 1;
+    yield 2;
+    yield 3;
+};
+
+const generator = getFunction();
+const result = generator.next();
+console.log(result);
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(result);
+
+let obj = {
+    start: 1,
+    end: 5,
+    *[Symbol.iterator]() {
+        for (let cnt = this.start; cnt <= this.end; cnt++) {
+            yield cnt;
+        }
+    },
+};
+
+console.log([...obj]);
